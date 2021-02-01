330b6fef20c50a4532d46da5d90bb54904705549
Brendan Leal <brendan.leal@outlook.com>
2021-02-01 14:34:00 -0700

    add filter and foreach methods

--- to-do-list.js
+++ to-do-list.js
@@ -104,7 +104,45 @@ class TodoList {
     console.log(`---- ${this.title} ----`);
     this.todos.forEach(todo => console.log(todo.toString()));
   }
+
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+
+  filter(callback) {
+    let filteredList = new TodoList(this.title);
+    this.forEach(todo => {
+      if (callback(todo)) filteredList.add(todo);
+    });
+
+    return filteredList;
+  }
 }
+
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
+
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+list.add(todo4)
+list.add(todo5)
+list.add(todo6)
+todo1.markDone();
+todo5.markDone();
+
+let doneTodos = list.filter(todo => todo.isDone());
+console.log(doneTodos);
+
+
+console.log(list.filter(todo => todo.isDone()).first());
+// => Todo { title: 'Buy milk', done: true }
 
 
 
